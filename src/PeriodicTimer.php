3047bad76f7d91a8c059f3d493c2e52b1fbbcbcb
421p <andrew.bakal@gmail.com>
2017-07-10 09:00:32 +0300

    timers

--- src/PeriodicTimer.php
+++ src/PeriodicTimer.php
@@ -2,7 +2,50 @@
 
 namespace Stimer;
 
-class PeriodicTimer
-{
+use React\EventLoop\LoopInterface;
 
+class PeriodicTimer extends Timer
+{
+    /** @var  Timer */
+    private $internalTimer;
+
+    public function __construct(LoopInterface $loop, $interval, callable $callback)
+    {
+        parent::__construct($loop, $interval, $callback);
+        $this->isPeriodic = true;
+    }
+
+    public function pause()
+    {
+        $this->internalTimer->pause();
+    }
+
+    public function resume()
+    {
+        $this->internalTimer->resume();
+    }
+
+    protected function start()
+    {
+        $this->internalTimer = $this->createInternalTimer();
+    }
+
+    public function cancel()
+    {
+        $this->internalTimer->cancel();
+    }
+
+    public function getLeftInterval(): float
+    {
+        return $this->internalTimer->getLeftInterval();
+    }
+
+    private function createInternalTimer(): Timer
+    {
+        return new Timer($this->getLoop(), $this->getInterval(), function () {
+            $this->getCallback()();
+            $this->internalTimer->cancel();
+            $this->internalTimer = $this->createInternalTimer();
+        });
+    }
 }
--- src/Timer.php
+++ src/Timer.php
@@ -2,7 +2,105 @@
 
 namespace Stimer;
 
-class Timer
-{
+use React\EventLoop\LoopInterface;
+use React\EventLoop\Timer\TimerInterface;
 
+class Timer implements TimerInterface
+{
+    protected $isPeriodic = false;
+
+    private $loop;
+    private $interval;
+    private $callback;
+
+    private $data;
+
+    private $isActive = false;
+
+    private $leftInterval;
+    private $startTime;
+
+    /** @var  TimerInterface */
+    private $internalTimer;
+
+    public function __construct(LoopInterface $loop, float $interval, callable $callback)
+    {
+        $this->loop = $loop;
+        $this->interval = $interval;
+        $this->callback = $callback;
+
+        $this->leftInterval = $interval;
+
+        $this->start();
+    }
+
+    public function getLoop(): LoopInterface
+    {
+        return $this->loop;
+    }
+
+    public function getInterval(): float
+    {
+        return $this->interval;
+    }
+
+    public function getCallback(): callable
+    {
+        return $this->callback;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function isPeriodic(): bool
+    {
+        return $this->isPeriodic;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function cancel()
+    {
+        $this->isActive = false;
+        $this->internalTimer->cancel();
+    }
+
+    public function pause()
+    {
+        $this->loop->futureTick(function () {
+            $this->isActive = false;
+            $this->internalTimer->cancel();
+
+            $elapsed = microtime(true) - $this->startTime;
+            $this->leftInterval -= $elapsed;
+        });
+    }
+
+    public function resume()
+    {
+        $this->loop->futureTick(function () {
+            $this->start();
+        });
+    }
+
+    protected function start()
+    {
+        $this->startTime = microtime(true);
+        $this->internalTimer = $this->loop->addTimer($this->leftInterval, $this->callback);
+    }
+
+    public function getLeftInterval(): float
+    {
+        return $this->leftInterval;
+    }
 }
